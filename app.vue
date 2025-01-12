99b88f14a02a20c4ec4609cd84e1d62b6126cd1d
Moonman <seagame.std@gmail.com>
2025-01-12 22:31:46 +0700

    update header text in app.vue

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     <UCard class="mt-10">
       <template #header>
         <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
+          <h1>Welcome to Nuxt UI Starter Edited from outside</h1>
           <ColorScheme
             ><USelect
               v-model="$colorMode.preference"
